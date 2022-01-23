71706c4e8b05c8bb45e7e36f33298dd4845d0de9
Shladeg <Shladeq@gmail.com>
2022-01-23 19:57:21 +0300

    game initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,27 @@
-import 'package:flutter_game_tutorial/game_core/main_loop.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main(List<String> args) {
-  startLoop();
+  WidgetsFlutterBinding.ensureInitialized();
+
+  SystemChrome.setPreferredOrientations(
+          [DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight])
+      .whenComplete(() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+
+    runApp(const MyApp());
+  });
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Center(
+          child: Text("GameW"),
+        ));
+  }
 }
